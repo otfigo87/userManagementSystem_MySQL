978824c42728f1bfc9fa6721e38d22ef3c33f7b1
Otmane Aatik <aaotmane@gmail.com>
2023-05-28 13:08:50 -0400

    bodyParser

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+require('dotenv').config;
+
 const app = express();
 const port = process.env.PORT || 3001;
+
+//Parsing middleware
+//Parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false}));
+//Parse application/json
+app.use(bodyParser.json());
 
 
 
