9099d9ea4922779bcea528aa9cad9ec9fcb24285
Otmane Aatik <aaotmane@gmail.com>
2023-05-30 22:35:31 -0400

    mysql connection

--- app.js
+++ app.js
@@ -21,6 +21,23 @@ app.use(express.static('public'));
 app.engine('hbs', exphbs.engine( {extname: '.hbs' }));
 app.set('view engine', 'hbs');
 
+// Connection Pool
+const pool = mysql.createPool({
+  connectionLimit: 100,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  user: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+// Connect to DB
+pool.getConnection((err, connection) => {
+    if(err){
+        throw err; // Not Connected
+    } else {
+        console.log("Connected as ID" + connection.threadId);
+    }
+})
+
 // Router
 app.get('', (req, res) => {
     res.render('home')
